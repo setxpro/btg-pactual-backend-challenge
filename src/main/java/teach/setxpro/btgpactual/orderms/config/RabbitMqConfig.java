f7d2ee2cf4e3c6e3b319852187d1bca01a67f7b8
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-06-22 08:34:04 -0300

    feat: config rabbitmq

---
+++ src/main/java/teach/setxpro/btgpactual/orderms/config/RabbitMqConfig.java
@@ -0,0 +1,24 @@
+package teach.setxpro.btgpactual.orderms.config;
+
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+
+    // Conversion Payload to JSON
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Declarable orderCreatedQueue() {
+        return new Queue(ORDER_CREATED_QUEUE);
+    }
+}
